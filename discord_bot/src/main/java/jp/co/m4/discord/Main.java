4786b8c06574bff66c768cb343859d2ccc8ee55e
panini <panpanini@mercari.com>
2021-01-16 12:22:35 +0900

    use env variable instead of arguments

--- discord_bot/src/main/java/jp/co/m4/discord/Main.java
+++ discord_bot/src/main/java/jp/co/m4/discord/Main.java
@@ -14,7 +14,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        String token = args[0];
+        String token = System.getenv("TOKEN");
         new Main(token);
     }
 }
